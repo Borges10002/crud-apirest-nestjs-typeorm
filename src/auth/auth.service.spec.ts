94f20ee8211e7e1f35647bdcd6e7b940f674f38f
Borges10002 <borges10002@gmail.com>
2025-03-30 20:28:54 -0400

    update

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,6 +4,8 @@ import { mailerServiceMock } from '../testing/mailer-service.mock';
 import { userRepositoryMock } from '../testing/user-repository.mock';
 import { userServiceMock } from '../testing/user-service.mock';
 import { AuthService } from './auth.service';
+import { userEntityList } from '../testing/use-entity-list-mock';
+import { acessToken } from '../testing/access-token.mock';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -21,4 +23,18 @@ describe('AuthService', () => {
 
     authService = module.get<AuthService>(AuthService);
   });
+
+  test('Validar a definição', () => {
+    expect(authService).toBeDefined();
+  });
+
+  describe('Token', () => {
+    test('createToken method', () => {
+      const result = authService.createToken(userEntityList[0]);
+
+      expect(result).toEqual({ acessToken });
+    });
+  });
+
+  //describe('Autenticação', () => {});
 });
---
+++ src/testing/jwt.payload.mock.ts
@@ -0,0 +1,10 @@
+export const jwtPayload = {
+  id: 1,
+  name: 'Miguel Athur ssss',
+  email: 'miguel@gmail.com',
+  iat: 1743371085,
+  exp: 1743975885,
+  aud: 'users',
+  iss: 'login',
+  sub: '1',
+};
